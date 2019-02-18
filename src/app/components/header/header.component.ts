1b594fe3a7ec27e6abbb22985cbb39f906ee988e
codexmaker <codexmaker12@gmail.com>
2019-02-18 14:02:18 -0600

    updated version modules

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `<h1>Header component</h1>`
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
